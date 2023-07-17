2c2ced68975b0def70a3be3b819de062a0071223
Omid Reza Heidari <omid77.orh@gmail.com>
2023-07-17 04:22:27 +0330

    Fix some typo + add alt to some icons

--- components/About.vue
+++ components/About.vue
@@ -11,14 +11,8 @@
 export default {
 	data(){
 		return{
-			'text':'Omid Reza Heidari is pursuing his master\'s studies in computer science at <a href="https://www.concordia.ca/" class="link-primary text-decoration-none">Concordia University</a>, advised by <a href="https://users.encs.concordia.ca/~wayang/" class="link-primary text-decoration-none">Professor Yang Wang</a>. Prior to that, he concluded his undergraduate studies at Islamic Azad University. He was a teaching assistant during his undergraduate studies at several universities, namely Sharif University of Technology, Zanjan University, and Islamic Azad University. His area of interest includes machine learning, deep learning databases, and data visualization.'
+			'text':'Omid Reza Heidari is pursuing his master\'s studies in computer science at <a href="https://www.concordia.ca/" class="link-primary text-decoration-none">Concordia University</a>, advised by <a href="https://users.encs.concordia.ca/~wayang/" class="link-primary text-decoration-none">Professor Yang Wang</a>. Prior to that, he concluded his undergraduate studies at Islamic Azad University. He was a teaching assistant during his undergraduate studies at several universities, namely Sharif University of Technology, Zanjan University, and Islamic Azad University. His area of interest includes machine learning, deep learning, databases, and data visualization.'
 		}
 	}
 }
 </script>
-
-<style scoped>
-a{
-
-}
-</style>
--- components/Contact.vue
+++ components/Contact.vue
@@ -3,14 +3,14 @@
 		<div class="alert alert-info">
 			<div class="d-flex justify-content-center">
 	      		<a class="px-3" v-for="section in sections" :href="section.link">
-					<img class="ico icon" :src="section.source"/>
+					<img class="ico icon" :src="section.source" :alt="section.alt" />
 		        </a>
 	      	</div>
 		</div>
 	</div>
 </template>
 
-<style>
+<style scoped>
 .icon{
 	width: 70px;
 	height: 70px;
@@ -24,23 +24,28 @@ export default {
 			sections:{
 				gamil:{
 					source: 'https://img.icons8.com/bubbles/1000/gmail-new.png',
-					link:'mailto:omid.orh@gmail.com'
+					link:'mailto:omid.orh@gmail.com',
+					alt:'Email'
 				},
 				linkedin:{
 					source: 'https://img.icons8.com/bubbles/1000/linkedin.png',
-					link:'https://www.linkedin.com/in/omidrezaheidari/'
+					link:'https://www.linkedin.com/in/omidrezaheidari/',
+					alt:'LinkedIn'
 				},
 				github:{
 					source: 'https://img.icons8.com/bubbles/1000/github.png',
-					link:'https://www.github.com/omid-reza'
+					link:'https://www.github.com/omid-reza',
+					alt:'GitHub'
 				},
 				twitter:{
 					source:'https://img.icons8.com/bubbles/1000/twitter-circled.png',
-					link:'https://twitter.com/omid_reza_'
+					link:'https://twitter.com/omid_reza_',
+					alt:'Twitter'
 				},
 				medium:{
 					source: 'https://img.icons8.com/bubbles/1000/medium-new.png',
-					link:'https://medium.com/@omidreza'
+					link:'https://medium.com/@omidreza',
+					alt:'Medium'
 				}
 			}
 		}
